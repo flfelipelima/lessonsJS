7fdc133541be2310cf2526643d85f166b5cf8b68
flfelipelima <mota.felipelima@gmail.com>
2023-12-18 17:15:34 -0300

    DOM, window, document, querySelector

---
+++ lesson8-DOM.js
@@ -0,0 +1,21 @@
+// Retorne o url da página atual utilizando o objeto window
+const local = window.location.href;
+console.log(local);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const elementoAtivo = document.querySelector(".ativo");
+console.log(elementoAtivo);
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela
+const largura = window.innerWidth;
+console.log(largura);
+
+// Aprendizado:
+// Manipulação de DOM
+// Objetos do DOM: windows e document
+// querySelector
